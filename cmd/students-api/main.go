b63773f967290faf3e7f61580c93cd0f0ea9e521
z1shivam <shivamk.json@gmail.com>
2025-05-30 11:56:56 +0530

    write graceful shutdown logic

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/z1shivam/learning-go/internal/config"
 )
@@ -25,9 +31,28 @@ func main() {
 		Handler: router,
 	}
 
-	fmt.Printf("Server started at port: %s", cfg.Addr)
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Fatal("Failed to start server.")
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		fmt.Printf("Server started at port: %s", cfg.Addr)
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("Failed to start server.")
+		}
+	}()
+
+	<-done
+
+	slog.Info("Shutting down the server")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// graceful shutdown
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("Failed to shutdown the server.", slog.String("error", err.Error()))
 	}
+
+	slog.Info("server shutdown successfully!")
 }
