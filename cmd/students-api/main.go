25c52ddcc27facf046971804a92ed08514a24d81
z1shivam <shivamk.json@gmail.com>
2025-05-30 11:39:20 +0530

    write server code

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/z1shivam/learning-go/internal/config"
+)
 
 func main() {
-	fmt.Println("code is working")
+	// load config
+	cfg := config.MustLoad()
+
+	// database setup
+	// setup router
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome to students api"))
+	})
+
+	// setup server
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	fmt.Printf("Server started at port: %s", cfg.Addr)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("Failed to start server.")
+	}
 }
